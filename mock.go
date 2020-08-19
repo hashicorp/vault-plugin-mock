2e56430125b9c990487ae4d51c56e333bb11606a
Sarah Thompson <sthompson@hashicorp.com>
2020-08-19 10:30:36 +0100

    Update mock.go

--- mock.go
+++ mock.go
@@ -1,11 +1,5 @@
 package mock
 
-import (
-	"fmt"
-	"github.com/hashicorp/vault/api"
-	"github.com/hashicorp/vault/sdk/database/dbplugin"
-)
-
 const (
 	MOCK = "This is a mock module to test plugin integration with Vault."
 )
