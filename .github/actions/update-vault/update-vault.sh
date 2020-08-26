eafe1ad393409b9a4fc153c6fcaa72bb8069da4f
sarahethompson <sthompson@hashicorp.com>
2020-08-26 15:52:08 +0100

    no message

--- .github/actions/update-vault/update-vault.sh
+++ .github/actions/update-vault/update-vault.sh
@@ -9,7 +9,7 @@ git config user.email github-actions@github.com
 git config --global url."https://$GITHUB_TOKEN:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 git checkout -b update-"$GITHUB_REPOSITORY"-"$VERSION"-"$(date +%s)"
 
-go get github.com/"$GITHUB_REPOSITORY-$semvar_bump"
+go get github.com/"$GITHUB_REPOSITORY$semvar_bump"
 go mod tidy
 rm -rf vendor
 go mod vendor
