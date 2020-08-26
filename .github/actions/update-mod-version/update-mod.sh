8be671630ad93882c7ea071bc40288df5205d2b7
sarahethompson <sthompson@hashicorp.com>
2020-08-26 11:58:02 +0100

    Restructuring action.

--- .github/actions/update-mod-version/update-mod.sh
+++ .github/actions/update-mod-version/update-mod.sh
@@ -4,12 +4,19 @@ set -euo pipefail
 branch=$1
 semvar_bump=$2
 
+# echo "Remote is - $(git remote -v)"
+git_remote=$(git config --get remote.origin.url)
+searchstring="https://github.com/"
+git_remote=${git_remote#*$searchstring}
+git_remote=${git_remote%".git"}
+echo "$git_remote"
+
 git config user.name github-actions
 git config user.email github-actions@github.com
 git config --global url."https://$GITHUB_TOKEN:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 git checkout -b update-"$GITHUB_REPOSITORY"-"$VERSION"-"$(date +%s)"
 
-go get github.com/hashicorp/vault-plugin-mock"$semvar_bump"
+go get github.com/"$GITHUB_REPOSITORY$semvar_bump"
 go mod tidy
 rm -rf vendor
 go mod vendor
@@ -17,8 +24,8 @@ go mod vendor
 if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
   git add .
   git commit -m "Updating $GITHUB_REPOSITORY deps"
-  command=$(hub pull-request -m "Update version of $GITHUB_REPOSITORY." -b "sarahethompson/vault:$branch" \
-  -h "sarahethompson/vault:update-$GITHUB_REPOSITORY-$VERSION-$(date +%s)" \
+  command=$(hub pull-request -m "Update version of $GITHUB_REPOSITORY." -b "$git_remote:$branch" \
+  -h "$git_remote:update-$GITHUB_REPOSITORY-$VERSION-$(date +%s)" \
   -l "plugin-update" -a "$ACTOR" -p | tail -1) || return 1
   echo "$command"
   text="<$command|PR on Vault $branch> successfully created! ($GITHUB_REPOSITORY version: $VERSION) and assigned to $ACTOR"
