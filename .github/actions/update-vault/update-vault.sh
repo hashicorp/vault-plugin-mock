fe765d6a3e3a98b5ea76dd8d8d3838546fa77a07
sarahethompson <sthompson@hashicorp.com>
2020-08-30 22:13:50 +0100

    no message

--- .github/actions/update-vault/update-vault.sh
+++ .github/actions/update-vault/update-vault.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 branch=$1
 semvar_bump=$2
 
+echo Remote is - $(git remote -v)"
 git_remote=$(git remote get-url origin | cut -f2 -d":")
 git_remote=${git_remote%".git"}
 echo "$git_remote"
